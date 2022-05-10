4ffcd1e80ac52ae45d8f3f36987a618280de0e00
Sunday Obinna Oruwhone <sundayamosobi@gmail.com>
2022-05-11 00:36:09 +0100

    Added FactsLists() to App.js

--- src/App.js
+++ src/App.js
@@ -19,6 +19,20 @@ function Title() {
   )
 }
 
+function FactsList() {
+  return (
+    <div>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
