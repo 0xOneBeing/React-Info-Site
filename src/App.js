b3a72ec44b487525cd8ee4ddc4c2d48c6a670cc5
Sunday Obinna Oruwhone <sundayamosobi@gmail.com>
2022-05-11 00:31:29 +0100

    Added HeaderImage() and Title() to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+function HeaderImage() {
+  return (
+    <div>
+      <img src={logo} width="200" />
+    </div>
+  )
+}
+
+function Title() {
+  return (
+    <div>
+      <h1>
+        Fun facts about React
+      </h1>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
@@ -9,12 +27,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
